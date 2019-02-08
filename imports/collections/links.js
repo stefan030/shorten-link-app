43abd6fbbbccbea2e595c95070a726707b503369
stelle030 <stefanbogdanovic030@gmail.com>
2019-02-08 11:22:56 +0100

    Token generator and url save to database

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -9,6 +9,17 @@ Meteor.methods({
             // If we return truthy value from arrow func, then check passes and its a valid input
             // If we return falsy check throws an error
        check(url, Match.Where(url => validUrl.isUri(url)));
+
+       // If it pass, we are ready to save the URL
+       // Generate token
+       const token = Math.random().toString(36).slice(-5);
+       // Save both url and token to the db
+       Links.insert({
+           url: url,
+           token: token,
+           clicks: 0
+       });
+
    }
 });
 
