99714e7c516002c5d07ca6482ab9a38b1785e74f
stelle030 <stefanbogdanovic030@gmail.com>
2019-02-08 12:28:35 +0100

    User redirect to long links

--- server/main.js
+++ server/main.js
@@ -10,11 +10,26 @@ Meteor.startup(() => {
     });
 });
 
+// Executed whenever user visits with a route like
+// 'localhost:3000/abcd'
+function onRoute(req, res, next) {
+    // Take the token out of the url and try to find a matching link in Links collection
+    const link = Links.findOne({ token: req.params.token });
+
+    if (link) {
+        // If we find link object, redirect the user to the long URL
+        res.writeHead(307, { 'Location': link.url });
+        res.end();
+    } else {
+        // If we don't find a link object, send the user to our normal React app
+        next();
+    }
+
+}
+
 const middleware = ConnectRoute(function(router) {
     // Looks for '/:token' request, if it matches, it will run the func
-   router.get('/:token', (req) => {
-      return console.log(req);
-   });
+   router.get('/:token', onRoute);
 });
 
 WebApp.connectHandlers.use(middleware);
