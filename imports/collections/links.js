553e03848fd1770ed94ebd07a06b185274b0edb1
stelle030 <stefanbogdanovic030@gmail.com>
2019-02-08 11:04:47 +0100

    Link validation via valid-url and Meteor's check and match

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,9 +1,14 @@
 import { Mongo } from 'meteor/mongo';
+import validUrl from 'valid-url';
+import { check, Match } from 'meteor/check';
 
 
 Meteor.methods({
-   'links.insert': function (url)  {
-       console.log('Attempting to save ' + url);
+   'links.insert': function(url)  {
+       // Match.Where => Used whenever we want to define custom validation
+            // If we return truthy value from arrow func, then check passes and its a valid input
+            // If we return falsy check throws an error
+       check(url, Match.Where(url => validUrl.isUri(url)));
    }
 });
 
