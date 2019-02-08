44b5a2040c822d72266aee1c8547d194e69d1d0c
stelle030 <stefanbogdanovic030@gmail.com>
2019-02-08 12:09:57 +0100

    Find tokens in url, implementation of WebApp and ConnectRoute

--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Links } from './../imports/collections/links';
+import { WebApp } from 'meteor/webapp'; // WebApp object => server component of Meteor - handles incoming requests and figures out what to do with them
+import ConnectRoute from 'connect-route'; // Creates a middleware that will take incoming HTTP request.
 
 
 Meteor.startup(() => {
@@ -7,3 +9,12 @@ Meteor.startup(() => {
        return Links.find({});
     });
 });
+
+const middleware = ConnectRoute(function(router) {
+    // Looks for '/:token' request, if it matches, it will run the func
+   router.get('/:token', (req) => {
+      return console.log(req);
+   });
+});
+
+WebApp.connectHandlers.use(middleware);
