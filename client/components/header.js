3a119f1ea1a4b7676f2206c97d5426bd46e2b631
stelle030 <stefanbogdanovic030@gmail.com>
2019-02-08 09:44:39 +0100

    Boilerplate set up

---
+++ client/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>
+            <nav className='nav navbar-default'>
+                <div className='navbar navbar-header'>
+                    <a className='navbar-brand'>Shorten My Link</a>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ imports/ui/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './../../client/components/header';
+
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>
+        )
+    }
+}
