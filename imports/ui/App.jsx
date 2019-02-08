795ee0198772ab56661924f1c1fd631df782d881
stelle030 <stefanbogdanovic030@gmail.com>
2019-02-08 11:33:30 +0100

    Publication for links - publish and subscribe set up

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './../../client/components/header';
 import LinkCreate from './../../client/components/link_create';
 import { Links } from './../../imports/collections/links';
+import LinkList from './../../client/components/link_list';
 
 
 export default class App extends React.Component {
@@ -10,6 +11,7 @@ export default class App extends React.Component {
             <div>
                 <Header />
                 <LinkCreate />
+                <LinkList />
             </div>
         )
     }
